21c21a18b33c7d86543ff64f56fb312b1954f522
TangRJ <imanity14@126.com>
2015-07-29 02:02:17 +0800

    加入数据文件

--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 using namespace ogdf;
 
 int main(){
-Graph G;
-node a = G.newNode();
-node b = G.newNode();
-node c = G.newNode();
-G.newEdge(a, b);
-G.newEdge(b, c);
+    Graph G;
+    node a = G.newNode();
+    node b = G.newNode();
+    node c = G.newNode();
+    G.newEdge(a, b);
+    G.newEdge(b, c);
 
-GraphAttributes GA(G);
+    GraphAttributes GA(G);
 
-FMMMLayout fmmm;
-fmmm.call(GA);
+    FMMMLayout fmmm;
+    fmmm.call(GA);
 
-cout << GA.x(a) << ", " << GA.y(a) << "; \n";
-cout << GA.x(b) << ", " << GA.y(b) << "; \n";
-cout << GA.x(c) << ", " << GA.y(c) << "; \n";
-return 0;
+    cout << GA.x(a) << ", " << GA.y(a) << "; \n";
+    cout << GA.x(b) << ", " << GA.y(b) << "; \n";
+    cout << GA.x(c) << ", " << GA.y(c) << "; \n";
+    return 0;
 }
